3799d95f6d68f03d8a11fdfe0ab95a218127691c
Zbignev Antonovic <zb.antonovic@gmail.com>
2018-04-12 16:24:11 +0300

    created crud

--- src/Controller/CoordinateController.php
+++ src/Controller/CoordinateController.php
@@ -2,31 +2,89 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Coordinate;
+use App\Form\CoordinateType;
+use App\Repository\CoordinateRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use App\Repository\CoordinateRepository;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * @Route("/coordinate")
+ */
 class CoordinateController extends Controller
 {
-     /**
-     * @Route("/coordinate")
-      */
-     public function showCoordinateAction(CoordinateRepository $repository)
-     {
-        $coordArray = array();
-        foreach ($repository->findAll() as $coordinate) {
-            $tempArray = array(
-                'name' => $coordinate->getName(),
-                'address' => $coordinate->getAddress(),
-                'latitude' => $coordinate->getLatitude(),
-                'longitude' => $coordinate->getLongitude(),
-                );
-            $coordArray[] = $tempArray;
-        }
-        return new JsonResponse($coordArray); 	
+    /**
+     * @Route("/", name="coordinate_index", methods="GET")
+     */
+    public function index(CoordinateRepository $coordinateRepository): Response
+    {
+        return $this->render('coordinate/index.html.twig', ['coordinates' => $coordinateRepository->findAll()]);
     }
 
+    /**
+     * @Route("/new", name="coordinate_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $coordinate = new Coordinate();
+        $form = $this->createForm(CoordinateType::class, $coordinate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($coordinate);
+            $em->flush();
+
+            return $this->redirectToRoute('coordinate_index');
+        }
+
+        return $this->render('coordinate/new.html.twig', [
+            'coordinate' => $coordinate,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="coordinate_show", methods="GET")
+     */
+    public function show(Coordinate $coordinate): Response
+    {
+        return $this->render('coordinate/show.html.twig', ['coordinate' => $coordinate]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="coordinate_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Coordinate $coordinate): Response
+    {
+        $form = $this->createForm(CoordinateType::class, $coordinate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('coordinate_edit', ['id' => $coordinate->getId()]);
+        }
+
+        return $this->render('coordinate/edit.html.twig', [
+            'coordinate' => $coordinate,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="coordinate_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Coordinate $coordinate): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$coordinate->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($coordinate);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('coordinate_index');
+    }
 }
---
+++ src/Controller/MapCoordinateController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Repository\CoordinateRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class MapCoordinateController extends Controller
+{
+     /**
+     * @Route("/mapcoordinate")
+      */
+     public function showCoordinateAction(CoordinateRepository $repository)
+     {
+        $coordArray = array();
+        foreach ($repository->findAll() as $coordinate) {
+            $tempArray = array(
+                'name' => $coordinate->getName(),
+                'address' => $coordinate->getAddress(),
+                'latitude' => $coordinate->getLatitude(),
+                'longitude' => $coordinate->getLongitude(),
+                );
+            $coordArray[] = $tempArray;
+        }
+        return new JsonResponse($coordArray); 	
+    }
+
+}

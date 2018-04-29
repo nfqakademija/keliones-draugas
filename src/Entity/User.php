b632397767c341745ceb1e9678960c9afd109784
Aiste Jurevice <project.blon@gmail.com>
2018-04-29 22:38:25 +0300

    registracijos forma veikia, iraso ROLE_USER

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,12 @@ use FOS\UserBundle\Model\User as BaseUser;
  */
 class User extends BaseUser
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->roles = array('ROLE_USER');
+    }
     /**
      * @return array
      */

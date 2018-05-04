b1c8834f8f25211b9f4cdc2c4df8140dc2f6e748
Aiste Jurevice <project.blon@gmail.com>
2018-05-04 20:23:21 +0300

    lat, long perdavimas

--- src/Repository/CoordinateRepository.php
+++ src/Repository/CoordinateRepository.php
@@ -17,5 +17,17 @@ class CoordinateRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Coordinate::class);
+
+    }
+
+    public function getCoordinates($bottomLeftLat, $bottomLeftLng, $topRightLat, $topRightLng){
+
+        $query = "SELECT id, name, address, latitude, longitude
+                from coordinate
+                where $bottomLeftLat <= latitude AND latitude <= $bottomLeftLng
+                      and  $topRightLat <= longitude AND longitude <= $topRightLng;";
+        $connection = $this->getEntityManager()->getConnection();
+        $statment = $connection->executeQuery($query);
+        return $statment->fetchAll();
     }
 }

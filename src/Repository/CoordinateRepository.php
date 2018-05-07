aa9672c63be8afc85270a7df8c9874b81a22e879
Aiste Jurevice <project.blon@gmail.com>
2018-05-07 21:31:03 +0300

    added typehint, bindValue

--- src/Repository/CoordinateRepository.php
+++ src/Repository/CoordinateRepository.php
@@ -20,12 +20,45 @@ class CoordinateRepository extends ServiceEntityRepository
 
     }
 
+    /**
+     * @Assert\Type("float")
+     */
+
     public function getCoordinates($bottomLeftLat, $bottomLeftLng, $topRightLat, $topRightLng){
 
         $query = "SELECT id, name, address, latitude, longitude
                 from coordinate
-                where $bottomLeftLat <= latitude AND latitude <= $bottomLeftLng
-                      and  $topRightLat <= longitude AND longitude <= $topRightLng;";
+                where ? <= latitude AND latitude <= ?
+                      and ? <= longitude AND longitude <= ?";
+        $connection = $this->getEntityManager()->getConnection();
+        $stmt = $connection->prepare($query);
+        $stmt->bindValue(1, $bottomLeftLat);
+        $stmt->bindValue(2, $bottomLeftLng);
+        $stmt->bindValue(3, $topRightLat);
+        $stmt->bindValue(4, $topRightLng);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+//    public function getAllCoordinates(){
+//        $query ="SELECT id, name, address, latitude, longitude
+//                from coordinate, coordinate_type
+//                where coordinate_type.id = coordinate_type_id";
+//        $connection = $this->getEntityManager()->getConnection();
+//        $statment = $connection->executeQuery($query);
+//        return $statment->fetchAll();
+//    }
+
+    public function getCoordinateByType($type){
+        $query = "select * from coordinate
+                where coordinate_type_id=(select id from coordinate_type where type = '$type')";
+        $connection = $this->getEntityManager()->getConnection();
+        $statment = $connection->executeQuery($query);
+        return $statment->fetchAll();
+    }
+
+    public function getTypes(){
+        $query = 'select type from coordinate_type;';
         $connection = $this->getEntityManager()->getConnection();
         $statment = $connection->executeQuery($query);
         return $statment->fetchAll();

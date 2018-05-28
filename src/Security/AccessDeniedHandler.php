00dafeb2c9666ff711185ef71e97a6060b7a0114
Zbignev Antonovic <zb.antonovic@gmail.com>
2018-05-28 19:13:02 +0300

    '/coordinate' and '/coordinate-type' paths can only be accessed by admin

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $content = "Only admin has access to this page!";
+        return new Response($content, 403, array('content-type' => 'text/html'));
+    }
+}

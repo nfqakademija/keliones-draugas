ac0addbb4f38297a2b600fa46b9163c18f4c32c5
Zbignev Antonovic <zb.antonovic@gmail.com>
2018-05-31 15:26:51 +0300

    roles to /coordinate and /coordinate-type routes

--- src/Controller/CoordinateTypeController.php
+++ src/Controller/CoordinateTypeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\CoordinateType;
 use App\Form\CoordinateType1Type;
 use App\Repository\CoordinateTypeRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/coordinate-type")
+ * @Security("has_role('ROLE_ADMIN')")
  */
 class CoordinateTypeController extends Controller
 {
